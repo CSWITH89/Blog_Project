5408e111902659c619ea9dda8acf1e338bcf7270
avi312singh <avi312singh@gmail.com>
2019-01-18 10:31:59 +0000

    Working author name

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,52 +1,17 @@
-import React, {Fragment} from 'react'
-import {StaticQuery, graphql } from 'gatsby'
+import React, { Fragment } from 'react'
+import { Link, StaticQuery, graphql } from 'gatsby'
 
 
-const IndexPage = (data) => (
+const HomePage = ({ data }) => {
+  return <div>{data.contentfulAuthor.name}</div>
+}
 
-  <StaticQuery query={graphql`
-  query Posts{
-    allContentfulBlogPost{
-      edges{
-        node{
-          title
-          date
-          content{
-            content
-          }
-          image{
-            fixed{
-               base64
-         tracedSVG
-         aspectRatio
-         width
-         height
-         src
-         srcSet
-         srcWebp
-         srcSetWebp
-            }
-          }
-          authorName{
-            name
-          }
-          
-          
-        }
-      }
-    }
-  }
-  `}
-  render= { 
-    (data) => (
+export const query = graphql`
+         query authourName {
+           contentfulAuthor {
+             name
+           }
+         }
+       `
 
-    <Fragment>
-
-    <h1>{name}</h1>
-    <p>Created on {date}</p>
-    </Fragment>
-  )}
- />
-);
-
-export default IndexPage
+export default HomePage
